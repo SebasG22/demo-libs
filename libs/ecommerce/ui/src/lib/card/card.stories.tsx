a4cef5c84a2a8717a42487e518fa7e600c6f51a2
Sebastian Guevara <info@sebasg.dev>
2022-07-28 15:01:04 -0500

    feat(repo): adding constraints to eslint

---
+++ libs/ecommerce/ui/src/lib/card/card.stories.tsx
@@ -0,0 +1,19 @@
+import { Story, Meta } from '@storybook/react';
+import { Card, CardProps } from './card';
+
+export default {
+  component: Card,
+  title: 'Mycard',
+} as Meta;
+
+const Template: Story<CardProps> = (args) => <Card {...args} />;
+
+export const Basic = Template.bind({});
+Basic.args = {
+  title: 'Soy kfhdakhfka',
+};
+
+export const ConfigAdvance = Template.bind({});
+Basic.args = {
+  title: 'Soy config Advance',
+};
--- libs/ecommerce/ui/src/lib/card/card.tsx
+++ libs/ecommerce/ui/src/lib/card/card.tsx
@@ -1,10 +1,15 @@
+import { useProduct } from '@novaventa/ecommerce/data-access';
 /* eslint-disable-next-line */
-export interface CardProps {}
+export interface CardProps {
+  title: string;
+}
 
 export function Card(props: CardProps) {
+  const { message } = useProduct();
+  console.log(message);
   return (
     <div>
-      <h1>Welcome to Card!</h1>
+      <h1>Welcome to Card! {props.title}</h1>
     </div>
   );
 }
